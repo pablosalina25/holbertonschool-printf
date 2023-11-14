099a70a5ddcd4f2107bb66f25f5f0be5b9cdbf77
pablosalina25 <you@example.com>
2023-11-14 07:54:29 -0800

    test impressions

--- printf.c
+++ printf.c
@@ -4,30 +4,42 @@
 /** printf- : a function that produces output according to a format.
  * @format: the format string
  */
-int _printf(const char *format, ...);
+int _printf( char *format, ...)
 
 {
 	char *ptr = format;
-		if (format =! NULL)
+	fflush(stdout);
+		if (format != NULL)
 		{
 			return (-1);
 		}
 
 	while (*ptr)
 	{
-		if (*ptr == '%')
+		if (*ptr == '/' )
+		{
+			ptr++;
+		
+			if (ptr != NULL)
+				write (1 , ptr , 1);
+		}	
+		else if (*ptr == '%')
 		{		
-			ptr++
+			ptr++;
 			switch (*ptr)
 			{
 			case 'c':
+				write (1 , ptr , 2);
 		      	break;
 			case 's':
+			_puts("playsHolder");
 			break;
-			default:	
 			}
 		}
+		else
+			write (1 , ptr ,2);
+
 		ptr++;
 	}
-
+	return(0);
 }
